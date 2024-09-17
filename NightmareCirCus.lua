cccd94e23a87c652cbfdcedfe80f88d7f718faf7
x3Catsusz <158755815+MerryXTrash@users.noreply.github.com>
2024-09-17 23:26:16 +0700

    Update NightmareCirCus.lua

--- NightmareCirCus.lua
+++ NightmareCirCus.lua
@@ -2172,18 +2172,8 @@ end
 
 local id = game.PlaceId
 local TP = game.Players.LocalPlayer.Character
-
 local Workspace = game:GetService("Workspace")
 
-local function setHoldDurationForAllProximityPrompts()
-    for i, v in ipairs(game:GetService("Workspace"):GetDescendants()) do
-    if v.ClassName == "ProximityPrompt" then
-        v.HoldDuration = 0
-    end
-end
-end
-
-setHoldDurationForAllProximityPrompts()
 
 local function fire()
     for _, descendant in ipairs(Workspace:GetDescendants()) do
