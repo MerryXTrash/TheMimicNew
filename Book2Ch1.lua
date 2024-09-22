2fff6545f90a636417a52939c1840ab0b6088e2a
x3Catsusz <158755815+MerryXTrash@users.noreply.github.com>
2024-09-22 20:53:49 +0700

    Update Book2Ch1.lua

--- Book2Ch1.lua
+++ Book2Ch1.lua
@@ -2176,8 +2176,6 @@ local char = player.Character or player.CharacterAdded:Wait()
 local TP = char:WaitForChild("HumanoidRootPart")
 local Workspace = game:GetService("Workspace")
 
-function ResetGUI()
-
 local function fire()
     for _, descendant in ipairs(Workspace:GetDescendants()) do
         if descendant:IsA("ProximityPrompt") then
@@ -2481,7 +2479,7 @@ local function ToCFrame(targetPosition)
     local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
 
     local originalGravity = workspace.Gravity
-    local speed = 1000
+    local speed = 2000
     local isTweening = true
 
     workspace.Gravity = 0
@@ -2509,7 +2507,7 @@ if id == 8056702588 then
 local OfficeFunctions = MenuFunctions:AddTab('Office','Function','menu')
 local OfficeSec = OfficeFunctions:AddSection('Function','Enter','Auto Win','home')
 OfficeSec:AddButton('Enter Office',function(v)
-	TP.CFrame = CFrame.new(-1778.0726318359375, 9.717201232910156, -4295.62109375) * CFrame.new(0, 3, 0)
+	ToCFrame(Vector3.new(-1778.0726318359375, 9.717201232910156, -4295.62109375) * CFrame.new(0, 3, 0))
 end)
 end
 
@@ -2549,7 +2547,8 @@ TP.CFrame = CFrame.new(-1674.8272705078125, -21.01018524169922, -3402.3908691406
 wait(0.2)
 fire()
 fire()
-wait(1)
+end)
+
 MioSection:AddButton('Auto Rat',function(v)
 	Ratfind()
 end)
@@ -2563,7 +2562,8 @@ end)
 MioSection:AddButton('Run Away from Mio',function(v)
     To(Vector3.new(-961.4176635742188, -46.48267364501953, -3601.613525390625))
 end)
-end)
+
+
 
 
 --Nagisa
@@ -2577,16 +2577,17 @@ end)
 
 
 
+
 --Village
 VillageSection:AddButton('Talk',function(v)
-    TP.CFrame = CFrame.new(-323.47344970703125, 20.420881271362305, 3653.791748046875)
+    ToCFrame(Vector3.new(-323.47344970703125, 20.420881271362305, 3653.791748046875))
 	wait(0.2)
 	fire()
 end)
 
 VillageSection:AddDropdown('Select House', {'House 1','House 2','Drawing House','House 4','House 5'}, nil, 1, function(list, item)
     if item == 'House 1' then
-		TP.CFrame = CFrame.new(-2454.953369140625, -1.9218900203704834, 375.8285217285156)
+		ToCFrame(Vector3.new(-2454.953369140625, -1.9218900203704834, 375.8285217285156))
 	elseif 'House2' then
 
     end
@@ -2710,27 +2711,3 @@ VisualSection:AddButton('Fullbright',function(v)
         end
     end
 end)
-end
-
-ResetGUI()
-
-local player = game.Players.LocalPlayer
-
--- ฟังก์ชันที่ทำงานเมื่อผู้เล่นเกิดใหม่
-local function onCharacterAdded(character)
-    local mainGUI = game:GetService("CoreGui"):FindFirstChild("Main")
-        local toggleGUI = game:GetService("CoreGui"):FindFirstChild("Toggle")
-
-        if mainGUI then
-            mainGUI:Destroy()
-        end
-
-        if toggleGUI then
-            toggleGUI:Destroy()
-	end
-	wait(1)
-	ResetGUI()
-end
-
--- เรียกใช้งานเมื่อผู้เล่นเกิดใหม่
-player.CharacterAdded:Connect(onCharacterAdded)
