ee57e844f6b655a782fe1b1cb51aa36bf66a00ed
x3Catsusz <158755815+MerryXTrash@users.noreply.github.com>
2024-09-24 19:08:18 +0700

    Update Classic.lua

--- Extra/Classic.lua
+++ Extra/Classic.lua
@@ -2282,7 +2282,7 @@ local function UnEspPlayers()
 end
 end
 
-local Window = Alc:NewWindow('Overflow','The Mimic - Classic','rbxassetid://134204200422920')
+local Window = Alc:NewWindow('Overflow','The Mimic - Classic','rbxassetid://134754092492795')
 local MenuFunctions = Window:AddMenu('Genaral',"Main",'list','tab')
 local UpdateFunctions = Window:AddMenu('Update',"Update Log",'hash','tab')
 
