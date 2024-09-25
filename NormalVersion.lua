0d1655528a7bf6688683fc444f8d06fd429afe65
x3Catsusz <158755815+MerryXTrash@users.noreply.github.com>
2024-09-25 08:46:05 +0700

    Update NormalVersion.lua

--- NormalVersion.lua
+++ NormalVersion.lua
@@ -1,5 +1,14 @@
 local currentPlaceId = game.PlaceId
 
+function notify(Section, Description, num)
+    game.StarterGui:SetCore("SendNotification", {
+        Title = Section;
+        Text = Description;
+        Duration = num;
+    })
+end
+
+
 if currentPlaceId == 6296321810 or currentPlaceId == 6479231833 or currentPlaceId == 6301638949 or currentPlaceId == 6480994221 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Normal/Book1ch1.lua'))() --b1c1
 elseif currentPlaceId == 6373539583 or currentPlaceId == 6485055338 or currentPlaceId == 6406571212 or currentPlaceId == 6485055836 or currentPlaceId == 6425178683 or currentPlaceId == 6485056556 then
@@ -27,5 +36,5 @@ elseif currentPlaceId == 11126398230 then
 elseif currentPlaceId == 7618863566 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Normal/Jigoku.lua'))() --Jigoku
 else
-    print("No matching game found for this PlaceId.")
+    notify("Not Support", "Script not Support it Game.", 5)
 end
